2ef07ae8c9e4938bc7b3a0ea7756b3eb4d5bc472
fredeos <fobando@estudiantec.cr>
2024-05-12 15:44:55 -0600

    Manager initial structure
    Missing random generation of positions, genetic algorithm for enhanced bots, and mutex locks for security

--- Game/src/include/Manager.cpp
+++ Game/src/include/Manager.cpp
@@ -1,5 +1,85 @@
 #include "Manager.h"
 
+Manager::Manager(){
+    this->origin = nullptr;
+}
+
+Manager::Manager(Map2D* source, int specters, int eyes, int rats, int chocobos, int supers, int treasures, int vases){
+    // [ LOAD ENEMIES ]
+    this->origin = source;
+    int max_enemies = specters+eyes+rats+chocobos+supers;
+    for (int i = 0; i<max_enemies; ++i){
+        if ( i <= specters){
+            Enemy* specter = new Specter(0.f, 0.f);
+            source->locate_at(specter, 1, 1 , true);
+            this->mob_entities.push_back(specter);
+        }
+        if ( i <= eyes){
+            Enemy* eye = new Eye(0.f,0.f);
+            source->locate_at(eye, 1, 1 , true);
+            this->mob_entities.push_back(eye);
+        }
+        if ( i <= rats){
+            Enemy* rat = new Rat(0.f, 0.f);
+            source->locate_at(rat, 1, 1 , true);
+            this->mob_entities.push_back(rat);
+        }
+        if ( i <= chocobos){
+            Enemy* bird = new Chocobo(0.f, 0.f);
+            source->locate_at(bird, 1, 1 , true);
+            this->mob_entities.push_back(bird);
+        }
+        if ( i <= supers){
+            Enemy* super = new Super(0.f, 0.f);
+            source->locate_at(super, 1, 1 , true);
+            this->mob_entities.push_back(super);
+        }
+    }
+
+    // [ LOAD OTHER ENTITIES ]
+    int max_statics = treasures+vases;
+    for (int i = 0; i < max_statics; ++i){
+        if (i <= treasures){
+            Entity* treasure = new Treasure(0.f, 0.f);
+            source->locate_at(treasure, 4, 4 , true);
+            this->static_entities.push_back(treasure);
+        }
+        if (i <= vases){
+            Entity* vase = new Vase(0.f, 0.f);
+            source->locate_at(vase, 7, 3 , true);
+            this->static_entities.push_back(vase);
+        }
+    }
+}
+
+void Manager::addMapRef(Map2D* source){
+    this->origin = source;
+}
+
+void Manager::addEntities(EntityType entity_t, int quantity){
+
+}
+
+Entity* Manager::getEntity(Collection group, int index){
+    switch (group){
+    case enemies:
+        return this->mob_entities[index];
+    case statical:
+        return this->static_entities[index];
+    }
+    return nullptr;
+}
+
+int Manager::size(Collection group){
+    switch (expression){
+    case enemies:
+        return this->mob_entities.size();
+    case statical:
+        return this->static_entities.size();
+    }
+    return 0;
+}
+
 void Manager::control(){
     thread({
         
--- Game/src/include/Manager.h
+++ Game/src/include/Manager.h
@@ -3,6 +3,7 @@
 
 #include "Entity.h"
 #include "Enemy.h"
+#include "Map.h"
 #include "wrapper.hpp"
 
 #include <chrono>
@@ -16,18 +17,62 @@ enum Collection {
     statical
 }
 
+enum EntityType {
+    CHOCOBO,
+    EYE,
+    RAT,
+    SPECTER,
+    SUPER,
+    TREASURE,
+    VASE
+}
+
 /// @brief Class that takes care of generating the required entities for the game levels
 class Manager {
     private: // Attributes
-        std::vector<Enemies> enemies;
-        std::vector<Entity> statical;
+    // ---- [Collections of entities]
+        std::vector<Enemy*> mob_entities;
+        std::vector<Entity*> static_entities;
+
+        std::mutex entities_guard;
+    // ---- [Parameters for entity creation]
+        Map2D* origin;
     public: // Methods
+
+        /// @brief Default constructor
         Manager();
 
-        void add(Entity entity);
+        /// @brief Constructor for class
+        /// @param source: reference to the map where the entities should spawn
+        /// @param specters: number of specters to spawn
+        /// @param eyes: number of eyes to spawn
+        /// @param rats: number of rats to spawn
+        /// @param chocobos: number of chocobos to spawn
+        /// @param supers: number of supers to spawn
+        /// @param treasures: number of treasures to spawn
+        /// @param vases: : number of vases to spawn
+        Manager(Map2D* source, int specters, int eyes, int rats, int chocobos, int supers, int treasures, int vases);
+        
+        /// @brief Add a reference to a Map2D object in which the manager should spawn enemies
+        /// @param source: pointer to the map instance
+        void addMapRef(Map2D* source);
 
-        Entity& operator[](Collection vect);
+        /// @brief Adds a new group of entities to the management unit
+        /// @param type: entity type to add
+        /// @param quantity: amount of new entities to add
+        void addEntities(EntityType entity_t, int quantity);
 
+        /// @brief Gets a reference to an Entity managed by this instance
+        /// @param group: name of group to access
+        /// @param index: position
+        /// @return Pointer to an Entity; can be casted to requested type using 'dynamic_cast<...>' or 'static_cast<...>'
+        Entity* getEntity(Collection group, int index);
+
+        /// @brief Gets the size of the group of entities
+        /// @param group: name of group
+        int size(Collection group);
+
+        /// @brief Analyzes the results of the enemies and generates new behaviors/attributes
         void evolve();
     private:
         void control();
